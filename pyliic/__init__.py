9b2c65e4c28153bc4f65ff496bdb1e7a8894967a
tdymkowski@icloud.com <tdymkowski@icloud.com>
2026-06-16 19:31:10 +0200

    create geom functions

--- pyliic/__init__.py
+++ pyliic/__init__.py
@@ -4,6 +4,7 @@ from .geometry_generator import GeometryGenerator
 from .fileio import write_xyz_traj
 from .eckart import apply_eckart
 from .gmatrix import get_G_matrix, get_G_element, reshape_positions_for_gmat
+from .pyliic import write_pyphspu_data, create_geometries_2D, create_geometries_phi, create_geometries_dr, create_geometries_ro2h, create_geometries_ro1h
 
 __all__ = [
         "XYZ",
@@ -16,7 +17,13 @@ __all__ = [
         "GeometryGenerator",
         "get_G_matrix",
         "get_G_element",
-        "reshape_positions_for_gmat"
+        "reshape_positions_for_gmat",
+        "create_geometries_ro1h",
+        "create_geometries_ro2h",
+        "create_geometries_dr",
+        "create_geometries_phi",
+        "create_geometries_2D",
+        "write_pyphspu_data",
 ]
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
--- pyliic/pyliic.py
+++ pyliic/pyliic.py
@@ -1,15 +1,211 @@
 #! /usr/bin/env python3
 import numpy as np
+from pathlib import Path
 import sys
-
+from os.path import join
 from pyliic.eckart import apply_eckart
-from .fileio import write_xyz_traj
+from .fileio import write_xyz, write_xyz_traj
 from .gmatrix import get_G_element, get_G_matrix, reshape_positions_for_gmat
 from .data import AMU2AU, ANG2AU
 from .interpolation import liic, make_distance_dihedral_grid
 from .utils import XYZ
 from .geometry_generator import GeometryGenerator
 
+N_IMAGES = 200
+GEOM_DATA_FILE = "geometry_data.txt"
+
+def read_pyphspu_input():
+    xyzpath = sys.argv[1]
+    tab = np.genfromtxt(sys.argv[2])
+    if tab.ndim > 1:
+        idx = tab[:, 0]
+        x = tab[:, 1]
+    else:
+        idx = np.array(tab[0])
+        x = np.array(tab[1])
+    outpath = sys.argv[3]
+    return xyzpath, outpath, idx, x
+
+
+def create_geometries_ro1h():
+    xyzpath, outpath, _, RO1H = read_pyphspu_input()
+    xyz_lst = XYZ.read_xyz(xyzpath)
+    react = xyz_lst[0]
+    prod = xyz_lst[1]
+    gen_licc = GeometryGenerator(
+        react=react,
+        prod=prod,
+        proton_idx=11,
+        atom1_idx=9,
+        atom2_idx=8,
+        q1="r1",
+        q2=None,
+        n_images=N_IMAGES,
+        int_method="pchip")
+    data = []
+    geoms = []
+    for r in RO1H:
+        geom_r = gen_licc(r)
+        geoms.append(geom_r)
+    geoms = apply_eckart(geoms)
+    for i, (geom, r) in enumerate(zip(geoms, RO1H)):
+        fname = f"sac_{i}.xyz"
+        path = join(outpath, fname)
+        write_xyz(path, geom)
+        data.append([path, r])
+    return data
+
+
+def create_geometries_ro2h():
+    xyzpath, outpath, _, RO2H = read_pyphspu_input()
+    xyz_lst = XYZ.read_xyz(xyzpath)
+    react = xyz_lst[0]
+    prod = xyz_lst[1]
+    gen_licc = GeometryGenerator(
+        react=react,
+        prod=prod,
+        proton_idx=11,
+        atom1_idx=9,
+        atom2_idx=8,
+        q1="r2",
+        q2=None,
+        n_images=N_IMAGES,
+        int_method="pchip")
+    data = []
+    geoms = []
+    for r in RO2H:
+        geom_r = gen_licc(r)
+        geoms.append(geom_r)
+
+    geoms = apply_eckart(geoms)
+
+    for i, (geom, r) in enumerate(zip(geoms, RO2H)):
+        fname = f"sac_{i}.xyz"
+        path = join(outpath, fname)
+        write_xyz(path, geom)
+        data.append([path, r])
+    return data
+
+
+def create_geometries_dr():
+    # r2 - r1
+    xyzpath, outpath, _, DR = read_pyphspu_input()
+    xyz_lst = XYZ.read_xyz(xyzpath)
+    react = xyz_lst[0]
+    prod = xyz_lst[1]
+    gen_licc = GeometryGenerator(
+        react=react,
+        prod=prod,
+        proton_idx=11,
+        atom1_idx=9,
+        atom2_idx=8,
+        q1="dr",
+        q2=None,
+        n_images=N_IMAGES,
+        int_method="pchip")
+    data = []
+    geoms = []
+    for dr in DR:
+        geom_r = gen_licc(dr)
+        geoms.append(geom_r)
+
+    geoms = apply_eckart(geoms)
+
+    for i, (geom, dr) in enumerate(zip(geoms, DR)):
+        fname = f"sac_{i}.xyz"
+        path = join(outpath, fname)
+        write_xyz(path, geom)
+        data.append([path, dr])
+    return data
+
+
+def create_geometries_phi():
+    # dihedral
+    xyzpath, outpath, _, PHI = read_pyphspu_input()
+    xyz_lst = XYZ.read_xyz(xyzpath)
+    react = xyz_lst[0]
+    prod = xyz_lst[1]
+    gen_phi = GeometryGenerator(
+        react=react,
+        prod=prod,
+        proton_idx=11,
+        atom1_idx=9,
+        q1=None,
+        q2="dihedral",
+        dihedral_indices=[1, 0, 6, 8],
+        moving_indices=[8, 7, 10, 11],
+        phi_min=0.0,
+        phi_max=360.0,
+        n_images=200,
+        int_method="pchip")
+    data = []
+    geoms = []
+
+    for i, phi in enumerate(PHI):
+        geom_r = gen_phi(phi)
+        geoms.append(geom_r)
+
+    geoms = apply_eckart(geoms)
+    for i, (geom, phi) in zip(geoms, PHI):
+        fname = f"sac_{i}.xyz"
+        path = join(outpath, fname)
+        write_xyz(path, geom)
+        data.append([path, phi])
+    return data
+
+
+def create_geometries_2D(q1="r1"):
+    xyzpath, outpath, _, X = read_pyphspu_input()
+    xyz_lst = XYZ.read_xyz(xyzpath)
+    react = xyz_lst[0]
+    prod = xyz_lst[1]
+    make_geom = GeometryGenerator(react, prod, proton_idx=11, atom1_idx=9, atom2_idx=8, dihedral_indices=[1, 0, 6, 8], moving_indices=[8, 7, 10, 11], q1=q1)
+    data = []
+    geoms = []
+    for r, phi in X:
+        geom_r_phi = make_geom(r, phi=phi)
+        geoms.append(geom_r_phi)
+    geoms = apply_eckart(geoms)
+    for i, (geom, (r, phi)) in enumerate(zip(geoms, X[:, 0])):
+        fname = f"sac_{i}.xyz"
+        path = join(outpath, fname)
+        write_xyz(path, geom)
+        data.append([path, [r, phi]])
+    return data
+
+
+def write_pyphspu_data(data, dirpath, filename="geometry_data.txt"):
+    dirpath = Path(dirpath)
+    dirpath.mkdir(parents=True, exist_ok=True)
+
+    outfile = dirpath / filename
+
+    with open(outfile, "w") as f:
+        for item in data:
+            if len(item) < 2:
+                raise ValueError(
+                    "Each data entry must contain at least outpath and one coordinate."
+                )
+
+            outpath = item[0]
+
+            if len(item) == 2:
+                coords = np.asarray(item[1], dtype=float).ravel()
+            else:
+                coords = np.asarray(item[1:], dtype=float).ravel()
+
+            if coords.size < 1 or coords.size > 4:
+                raise ValueError(
+                    f"Coordinates must have dimension 1–4. "
+                    f"Got dimension {coords.size} for {outpath}."
+                )
+
+            coord_str = " ".join(f"{x:>10.6f}" for x in coords)
+
+            f.write(f"{str(outpath):>40s} {coord_str}\n")
+
+    return outfile
+        
 
 def main():
     n_images = 100
