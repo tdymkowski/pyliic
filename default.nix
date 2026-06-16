a5128475a4b8d1c8c2254becbec53e14cf6d84e5
tdymkowski@icloud.com <tdymkowski@icloud.com>
2026-06-16 14:39:42 +0200

    default.nix

--- default.nix
+++ default.nix
@@ -3,6 +3,7 @@
 let
   pkgs = nixpkgs;
   pyqdngSrc = pkgs.fetchFromGitLab {
+    owner = "Markus Kowalewski"
     url = "gitlab.fysik.su.se/markus.kowalewski/pyqdng.git";
     rev = "cdda7a857a6b72e34939f9c172e9828d4b80e0a8";
     hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
